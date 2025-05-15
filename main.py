8c463f81f4d896deb9b65f55775323f7bfa5a9ff
sandera1 <sandera1@rose-hulman.edu>
2025-05-14 22:24:22 -0400

    updated HMTL for /home

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 Rose-Hulman Chemical Engineering Data server web API
 David Henthorn, Chemical Engineering, 2022
 """
+from plotly.validators.surface.contours.x import project
 
 CONST_NAME = "CHE PI Data Portal"
 CONST_VER = "0.10"
@@ -23,31 +24,21 @@ server = PI.PIServer()
 #We added this server definition here because we believe it is defined somewhere else when running in the terminal
 # and does not get used when running in the PyCharm console
 app = Flask(__name__)
-
+project = ""
 @app.route('/')
 @app.route('/home')
 def home():
     logging.info("New request for /home from %s", request.remote_addr)
     return render_template('home.html')
 
-
-@app.route('/download', methods=["POST"])
-def download():
-    project = request.form.get("project")
-    logging.info("Received download request for project %s from IP %s", project, request.remote_addr)
-    date = request.form.get("start")
-    start_time = request.form.get("starttime")
-    end_time = request.form.get("endtime")
-    interval = request.form.get("interval")
-
+@app.route('/instrumentation', methods=[ "GET" ])
+def instrumentation():
+    project = request.args.get("project")
+    logging.info("New request for /instrumentation from %s", request.remote_addr)
     project_num = "*" + project + "*"
-    project_start = date + " " + start_time
-    project_end = date + " " + end_time
+    logging.info("Received request for project %s from IP %s", project, request.remote_addr)
 
-    logging.info("Requested dataset with start time of %s, end time of %s, and interval of %s",
-                 project_start, project_end, interval)
-
-    if project_num == "*300*":
+    if project_num =="*300#":
         search_term = "*-3*"
     else:
         search_term = project_num
@@ -56,37 +47,66 @@ def download():
 
     if len(points) == 0:
         logging.error("Found no PI datapoints for search term %s", search_term)
-        return 'Found no PI datapoints for search term' + search_term
+        return render_template('instrumentation.html',
+                             error=f"Found no PI datapoints for search term: {search_term}")
+
     else:
         logging.info("Found %s PI points for project %s", len(points), project)
-        logging.info("Concatenating")
-        df = pd.concat([
-            point.interpolated_values(project_start, project_end, interval).to_frame(
-                point.name + ' ' + point.units_of_measurement)
-            for point in points], axis=1)
+        logging.info("Colecting instruments")
+        instruments = [f"{point.name}" for point in points]
+        return render_template('instrumentation.html', options=instruments)
 
-        logging.info("Cleaning and sorting scolumns")
-        df.index.rename('Timestamp', inplace=True)
-        df.sort_index(axis=1, inplace=True)
 
-        # The encoding is off. Temporary workaround is to write the dataframe as a CSV, which fixes the encoding. Then read it in.
-        df.to_csv("temp.csv")
-        df = pd.read_csv("temp.csv")
+@app.route('/download', methods=["POST"])
+def download():
+    selected_instruments = request.form.getlist("instruments")
+    #logging.info("Received download request for project %s from IP %s", project, request.remote_addr)
+    date = request.form.get("start")
+    start_time = request.form.get("starttime")
+    end_time = request.form.get("endtime")
+    interval = request.form.get("interval")
 
-        # We frequently see columns returned full of "Shutdown" comments because na instrument no longer works.
-        # If the entire column is full of those, delete the whole column.
 
-        #df.replace("Shutdown", np.nan, inplace=True)
-        logging.info("Replacing non-numeric entries") 
-        df.replace("Shutdown", float(np.nan), inplace=True)
-        df.dropna(how='all', axis=1, inplace=True)
+    project_start = date + " " + start_time
+    project_end = date + " " + end_time
 
-        response = make_response(df.to_csv(date_format='%H:%M:%S'))
-        csvname = 'AREA' + project + '-' + date + '.csv'
-        response.headers['Content-Disposition'] = 'attachment; filename=' + csvname
-        response.mimetype = 'text/csv'
-        logging.info("Sending CSV file %s over http with response info %s", csvname, response)
-        return response
+    points = server.search(selected_instruments)
+
+    logging.info("Requested dataset with start time of %s, end time of %s, and interval of %s",
+                 project_start, project_end, interval)
+
+
+   # logging.info("Found %s PI points for project %s", len(points), project)
+    logging.info("Concatenating")
+    df = pd.concat([
+        point.interpolated_values(project_start, project_end, interval).to_frame(
+            point.name + ' ' + point.units_of_measurement)
+        for point in points], axis=1)
+
+    logging.info("Cleaning and sorting scolumns")
+    df.index.rename('Timestamp', inplace=True)
+    df.sort_index(axis=1, inplace=True)
+
+    # The encoding is off. Temporary workaround is to write the dataframe as a CSV, which fixes the encoding. Then read it in.
+    df.to_csv("temp.csv")
+    df = pd.read_csv("temp.csv")
+
+    # We frequently see columns returned full of "Shutdown" comments because na instrument no longer works.
+    # If the entire column is full of those, delete the whole column.
+
+    #df.replace("Shutdown", np.nan, inplace=True)
+    logging.info("Replacing non-numeric entries")
+    df.replace("Shutdown", float(np.nan), inplace=True)
+    df.dropna(how='all', axis=1, inplace=True)
+
+    response = make_response(df.to_csv(date_format='%H:%M:%S'))
+    csvname = 'DATA FROM' + '-' + date + '.csv'
+    #We can change the name of the downloaded csv file by changing the above line of code
+
+    response.headers['Content-Disposition'] = 'attachment; filename=' + csvname
+    response.mimetype = 'text/csv'
+    logging.info("Sending CSV file %s over http with response info %s", csvname, response)
+    return response
     
 
 @app.route('/csv', methods=['GET'])
